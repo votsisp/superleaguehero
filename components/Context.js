932db5c51034b9651ecc3af9cd7cf75258595b87
votsisp <82116734+votsisp@users.noreply.github.com>
2023-03-16 14:36:28 +0100

    Update Context.js

--- components/Context.js
+++ components/Context.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
   const getdata = async () => {
     
     try {
-      let res = await fetch(`https://superleaguehero/api/users`, {
+      let res = await fetch(`https://superleaguehero.com/api/users`, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -33,7 +33,7 @@ export const AuthProvider = ({ children }) => {
 
   const signin = async (person) => {
     try {
-      const res = await fetch(`https://superleaguehero/api/users`, {
+      const res = await fetch(`https://superleaguehero.com/api/users`, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -59,7 +59,7 @@ export const AuthProvider = ({ children }) => {
 
   const signup = async (person) => {
     try {
-      let res = await fetch(`https://superleaguehero/api/newusers`, {
+      let res = await fetch(`https://superleaguehero.com/api/newusers`, {
         
         method: "POST",
         body: JSON.stringify(person),
@@ -73,7 +73,7 @@ export const AuthProvider = ({ children }) => {
 
   const addScores = async (person) => {
     try {
-      let res = await fetch(`https://superleaguehero/updateusers`, {
+      let res = await fetch(`https://superleaguehero.com/updateusers`, {
         headers: { 
           'Content-Type': 'application/json'
         },
@@ -89,7 +89,7 @@ export const AuthProvider = ({ children }) => {
 
   const addBet = async (person) => {
     try {
-      let res = await fetch(`https://superleaguehero/api/updatebets`, {
+      let res = await fetch(`https://superleaguehero.com/api/updatebets`, {
         headers: { 
           'Content-Type': 'application/json'
         },
